45f2d1cf2358dc686d2d50dc039b9bb65ca7e3a3
Jiss James <jissjames360@gmail.com>
2017-02-09 17:28:25 +0000

    Home page design complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,7 @@ class HomeController < ApplicationController
     @location = Geocoder.search('143.239.9.24').first.city # => "Cork"
     @companies = Company.near(@location, 50)
     @activities = Activity.near(@location, 50)
+    @users = User.near(@location, 50)
     # @results = @activities + @companies
 
   end
