34e361cd0ea18f8b80cc95c4460b8c96dac45881
Jiss James <jissjames360@gmail.com>
2017-02-11 15:14:10 +0000

    Production settings changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Mailing settings
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'https://fypamb.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
